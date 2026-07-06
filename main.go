adf4dfdde9c957fd4a6793950d04bf618b6e3e0b
dimastadeoo <dimastadeoo@gmail.com>
2026-07-06 10:54:42 +0700

    feat: Add initial go.mod and main.go files with circle area and circumference calculations

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func luasLingkaran(jari int) float32{
+	var phi float32
+	if (jari % 7 == 0){
+		phi = 22/7
+	}else {
+		phi = 3.14
+	}
+	result := phi * float32(jari) * float32(jari)
+	return result
+}
+
+func kelLingkaran(jari int) float32{
+	var phi float32
+	if (jari % 7 == 0){
+		phi = 22/7
+	}else{
+		phi = 3.14
+	}
+	result := 2 * phi * float32(jari)
+	return result
+}
+
+func main(){
+	fmt.Printf("Luas Lingkaran %f \n", luasLingkaran(21))
+	fmt.Printf("Luas Lingkaran %f \n", kelLingkaran(21))
+}
